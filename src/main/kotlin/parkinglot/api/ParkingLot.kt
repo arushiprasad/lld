34d9420ccff4ee8839c2fcd1de01781e84b0b4f1
arushiprasad <arushi.prasad@ny.email.gs.com>
2023-03-28 20:32:25 +0530

    Moving ticket to its own file

--- src/main/kotlin/parkinglot/api/ParkingLot.kt
+++ src/main/kotlin/parkinglot/api/ParkingLot.kt
@@ -31,19 +31,3 @@ enum class LotStatus{
     free,
     occupied
 }
-
-data class Ticket(
-    val id:Int,
-    val vehicleId: Int,
-    val lotId:Int,
-    val timeOfEntry:Instant,
-    val timeOfExit:Instant?,
-    val status:TicketStatus,
-    val amountDue:Int=0
-    // can take in a calculator, that decides how much should be paid
-)
-
-enum class TicketStatus{
-    paid,
-    unpaid
-}
